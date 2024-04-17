af61603cf246a53ce51a2c3d01074a67a17c4d6a
Huan Nguyen <huanidz@github.com>
2024-04-18 00:54:17 +0700

    keep working on data process

--- test.py
+++ test.py
@@ -1,31 +1,16 @@
 import torch
-from torchvision import models, transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import CIFAR10
+from components.model.AlprModel import AlprModel
+from components.losses.loss import AlprLoss
+from components.data.AlprData import AlprDataset 
 from time import perf_counter
-import time
+import numpy as np
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-#Set the pretrained parameter as True when loading the model to get the pretrained weights.
-model = models.resnet50(pretrained=True).to(device)
-print(f"==>> model: {model}")
-print(f'The model has {count_parameters(model):,} trainable parameters')
-
-print("Start warming up 100 iteration batch=1..")
-start_time = perf_counter()
-input = torch.randn((12, 3, 256, 256), dtype=torch.float32).to(device)
-output = model(input)
-print(f"==>> output.shape: {output.shape}")
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    print('Interrupted by user')
-    
-
-    
-end_time = perf_counter()
-elaps = end_time - start_time
-print(f"Finish warming up. Took {elaps}ms total. Average = {elaps}ms per iteration.")
-
+loader = AlprDataset(images_folder="./train_data/images", labels_folder="./train_data/labels", input_size=384)
+item = loader[0]
---
+++ utils/helper_func.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def IoU(tl_rect_A, br_rect_A, tl_rect_B, br_rect_B):
+
+        WH_Rect_A = br_rect_A - tl_rect_A
+        WH_Rect_B = br_rect_B - tl_rect_B
+	
+        intersection_wh = np.maximum(np.minimum(br_rect_A, br_rect_B) - np.maximum(tl_rect_A, tl_rect_B), 0.)
+        intersection_area = np.prod(intersection_wh)
+    
+        area1, area2 = (np.prod(WH_Rect_A), np.prod(WH_Rect_B))
+        union_area = area1 + area2 - intersection_area
+    
+        return intersection_area / union_area
+
+def IOU_centre_and_dims(mn_center, mn_rect_wh, bounding_rect_center, bounding_rect_wh):
+        return IoU(mn_center - mn_rect_wh / 2, \
+                        mn_center + mn_rect_wh / 2, \
+                        bounding_rect_center - bounding_rect_wh / 2, \
+                        bounding_rect_center + bounding_rect_wh / 2)
