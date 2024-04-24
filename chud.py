inference code & fix model output softmax

chud.py
@@ -1,23 +1,20 @@
-import cv2
+import torch
 import numpy as np
 
-# Load the image
-img = cv2.imread('./train_data/images/00011.jpg')
+a = np.array([[1,2,3],[4,5,6]])
 
-# Define the shift offsets (x, y)
-x_offset = 50
-y_offset = -30
+b = np.where(a > 2)
+# print(f"==>> b: {b}")
 
-# Get the image dimensions
-height, width = img.shape[:2]
+# Make a dummy 4x4x3 tensor with integer values between 0 and 100
+c = np.random.randint(0, 101, size=(4, 4, 3))
+# c = np.random.rand(4, 4, 3)
+d = c[:, 2, 2]
 
-# Create the translation matrix
-translation_matrix = np.float32([[1, 0, x_offset], [0, 1, y_offset]])
+# Random tensor with value range from -1 to 1
+d = np.random.randn(2, 4, 4)
+print(f"==>> d: {d}")
 
-# Warp the image using the translation matrix
-shifted_img = cv2.warpAffine(img, translation_matrix, (width, height), borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+e = torch.softmax(torch.from_numpy(d), dim=0)
+print(f"==>> e: {e}")
 
-# Display the shifted image
-cv2.imshow('Shifted Image', shifted_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()

components/model/AlprModel.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from ..extractor import AlprResBlock, ConvBlock
 
@@ -67,7 +68,7 @@ class AlprModel(nn.Module):
         x = self.res_block_8(x)
         x = self.res_block_9(x)
         
-        probs = self.out_probs(x)
+        probs = torch.softmax(self.out_probs(x), dim=1) # B, 2, H, W
         bbox = self.out_bbox(x)
         
         return probs, bbox

components/postprocess/PostProcess.py
@@ -1,18 +1,79 @@
 import numpy as np
 import torch
+from utils.helper_func import nms
 # Literally adopt from original repo
 
-def reconstruct(original_image, resized_image, predict_feature_map, output_shape, detection_threshold):
-    
-    net_stride 	= 2**4
-    side 		= ((208. + 40.)/2.)/net_stride # 7.75
 
-    predict_feature_map = predict_feature_map.squeeze()
+def get_pts_homogeneous(tlx, tly, brx, bry):
+    """
+        Alias for getRectPts in original repo
+    """    
+    return np.array([
+        [tlx, brx, brx, tlx],
+        [tly, tly, bry, bry],
+        [1.0, 1.0, 1.0, 1.0]
+    ], np.float32)
 
-    Probs = predict_feature_map[0, ...]
-    Affines = predict_feature_map[2:, ...]
+def reconstruct(original_image, resized_image, predict_feature_map, detection_threshold):
     
-    xx, yy = np.where(Probs > detection_threshold)
+    resized_H, resized_W, _ = resized_image.shape
+    
+    net_stride 	= 16 # 2**4 
+    side 		= ((resized_W + 40.)/2.)/net_stride # 7.75
+    
+    predict_feature_map = predict_feature_map.squeeze() # 9x24x24
+
+    Probs = predict_feature_map[0, ...] # 24x24 (first dim), The predict_feature_map[1, ...] is non-obj prob which is not used in post-process
+    Affines = predict_feature_map[2:, ...] # 6x24x24 (last dims from the second dim)
+    
+    yy_valid, xx_valid = np.where(Probs > detection_threshold)
+    
+    H_resized, W_resized, _ = resized_image.shape
+    MN = torch.Tensor([W_resized / net_stride, H_resized / net_stride])
+    
+    vxx = vyy = 0.5 # alpha
+    
+    base = torch.Tensor([[-vxx, -vyy, 1.],
+                      [ vxx, -vyy, 1.],
+                      [ vxx,  vyy, 1.],
+                      [-vxx,  vyy, 1.]]).T
+    labels = []
+
+    for i in range(len(yy_valid)):
+        y, x = yy_valid[i], xx_valid[i] # Potential bug
+        affine = Affines[:, y, x] # (6, )
+        prob = Probs[y, x] # (1, )
+ 
+        mn = torch.Tensor([float(x) + .5, float(y) + .5])
+
+        A = torch.reshape(affine,(2,3))
+        A[0,0] = max(A[0,0], 0.)
+        A[1,1] = max(A[1,1], 0.)
+
+        pts = torch.Tensor(A @ base)
+        pts_MN_center_mn = pts * side
+        pts_MN = pts_MN_center_mn + mn.reshape((2,1))
+
+        pts_prop = pts_MN / MN.reshape((2,1))
+
+        # print(f"==>> pts_prop: {pts_prop}")
+        # print(f"==>> prob: {prob}")
+        # raise Exception("haha")
+        if prob > 0 and prob < 1:
+            labels.append((pts_prop, prob))
+
+    print(f"==>> labels: {len(labels)}")
+    final_labels = nms(Labels=labels, iou_threshold=0.1)
+    TLps = []
+    
+    if len(final_labels) > 0:
+        final_labels.sort(key=lambda l: l[1], reverse=True) # Sort by prob
+        
+        for i, label in enumerate(final_labels):
+            TLps.append((label))
+            
+    return TLps
+    
     
     
  

main.py
@@ -8,16 +8,17 @@ from components.model.AlprModel import AlprModel
 from utils.util_func import count_parameters
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 
 model = AlprModel().to(device)
 print(f'The model has {count_parameters(model):,} trainable parameters')
 
 criteria = AlprLoss().to(device)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.0003)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 
-dataset = AlprDataset(images_folder="./train_data/vollmont_images", labels_folder="./train_data/vollmont_labels", input_size=384)
+dataset = AlprDataset(images_folder="./train_data/WPOD_Training_Dataset/images", labels_folder="./train_data/WPOD_Training_Dataset/labels", input_size=384)
 
-batch_size = 16
+batch_size = 8
 
 train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
@@ -52,3 +53,8 @@ for epoch in range(epochs):
             running_loss = 0.0
             if loss_per_batch < lowest_loss:
                 lowest_loss = loss_per_batch
+                
+    # Save the model each epoch
+    print("Saving model...")
+    torch.save(model.state_dict(), f"model_{epoch}.pth")
+    